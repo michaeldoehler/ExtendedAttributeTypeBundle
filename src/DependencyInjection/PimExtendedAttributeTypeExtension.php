cc40553b2663c51c9821b5b1f0e1e896d27766b1
Michael Döhler <michael.doehler@intellishop.ag>
2017-10-14 00:13:31 +0200

    [FIX] Akeneo 2.0 compatibility changes

--- src/DependencyInjection/PimExtendedAttributeTypeExtension.php
+++ src/DependencyInjection/PimExtendedAttributeTypeExtension.php
@@ -30,6 +30,7 @@ class PimExtendedAttributeTypeExtension extends Extension
         $loader->load('completeness.yml');
         $loader->load('form_types.yml');
         //$loader->load('denormalizers.yml');
+        $loader->load('factories.yml');
         $loader->load('providers.yml');
         $loader->load('query_builders.yml');
         $loader->load('updaters.yml');
